2b69a52e225806dfad5a337bbe2817f62e22b519
jdclaas <jd_claas@icloud.com>
2019-09-28 05:22:14 -0500

    still have some work to do

---
+++ src/resumeData.js
@@ -0,0 +1,85 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "J.D. Claas",
+    "role": "Developer",
+    "linkedinId":"joseph-claas",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/joseph-claas-6678a417a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jdclaas",
+          "className":"fa fa-github"
+        },
+      
+      ],
+    "aboutme":"I'm currently a student for a UW extention bootcamp learning to be a full-stack developer.",
+    "address":"1012 E Land Pl, Milwaukee WI",
+    "education":[
+      {
+        "UniversityName":"UW Extention Program",
+        "YearOfPassing":"2019",
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Ox Optimal",
+        "specialization":"Intern",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2019",
+      },
+     
+    ],
+    "skillsDescription":"Skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"jQuery"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"Mongo"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"'Merica 2020",
+        "description":"Project Description Search for candidates running in 2020. Bring back news articles, meme generator, map and/or event calendar, music associated, comparison tool, podcast search, etc. Target Market: 18-40 age",
+        "imgurl":"images/2020.png"
+      },
+      {
+        "name":"Upstream",
+        "description":"UpStream is a movie watchlist app that allows users to search for movies, add them to a watchlist, mark a movie as watched, and write a review.",
+        "imgurl":"images/upstream.png"
+      },
+      {
+        "name":"Fosterme",
+        "description":"Foster Me is a foster animal tracker that allows users to sign up for a foster, build a foster animal profile, and keep track of the status of their past fosters.",  
+        "imgurl":"images/foster.png"
+      },
+
+    ],
+    
+  }
+  
+  export default resumeData
